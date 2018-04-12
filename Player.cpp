30d257d446b2d39d480bd21ca0f8192f79d20959
maximombro <maximombro@gmail.com>
2018-04-12 10:39:54 -0400

    Created the Player class and functions

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,81 @@
-//
-// Created by Workstation2 on 4/10/2018.
-//
-
 #include "Player.h"
+
+/// Color Struct
+colorStruct::colorStruct(double redIn, double greenIn, double blueIn) {
+    red = redIn;
+    green = greenIn;
+    blue = blueIn;
+}
+
+/// Position Struct
+posStruct::posStruct(int xIn, int yIn) {
+    xPos = xIn;
+    yPos = yIn;
+}
+
+/// Player Class
+// Constructors
+Player::Player(colorStruct color, posStruct pos) : color(color), pos(pos) {
+    // Set Food and Water
+    foodLevel = 100;
+    waterLevel = 100;
+}
+
+// Methods
+void Player::move(moveDirection dir) {
+    // Variables
+    int moveAmount = 10;
+
+    // Switch to direction
+    switch(dir) {
+        case up:
+            pos.yPos += moveAmount;
+            break;
+
+        case down:
+            pos.yPos -= moveAmount;
+            break;
+
+        case left:
+            pos.xPos -= moveAmount;
+            break;
+
+        case right:
+            pos.xPos += moveAmount;
+            break;
+    }
+}
+
+// Getters
+posStruct Player::getPos() const {
+    return pos;
+}
+
+colorStruct Player::getColor() const {
+    return color;
+}
+
+double Player::getFoodLevel() const {
+    return foodLevel;
+}
+
+double Player::getWaterLevel() const {
+    return waterLevel;
+}
+
+// Setters
+void Player::changeFoodLevel(double change) {
+    foodLevel += change;
+}
+
+void Player::changeWaterLevel(double change) {
+    waterLevel += change;
+}
+
+void Player::setFoodLevel(double level) {
+    foodLevel = level;
+}
+
+void Player::setWaterLevel(double level) {
+    waterLevel = level;
+}
--- Player.h
+++ Player.h
@@ -1,7 +1,108 @@
 #ifndef CAMPINGSURVIVAL_PLAYER_H
 #define CAMPINGSURVIVAL_PLAYER_H
 
+/// Move Direction Enum
+enum moveDirection {up,down,left,right};
+
+/// Color Structure
+struct colorStruct {
+    double red;
+    double green;
+    double blue;
+
+    colorStruct(double redIn, double greenIn, double blueIn);
+};
+
+/// Position Structure
+struct posStruct {
+    int xPos;
+    int yPos;
+
+    posStruct(int xIn, int yIn);
+};
+
+/// Player Class
 class Player {
+private:
+    posStruct pos;
+    colorStruct color;
+    double foodLevel;
+    double waterLevel;
+
+public:
+    // Constructor
+    /**
+     * Requires: Color, Position
+     * Modifies: pos, color, foodLevel, waterLevel
+     * Effects: Builds the Player object
+    */
+    Player(colorStruct color, posStruct pos);
+
+    // Methods
+    /**
+     * Requires: Direction
+     * Modifies: pos
+     * Effects: Moves the player position in the direction of 'dir'
+    */
+    void move(moveDirection dir);
+
+    // Getters
+    /**
+     * Requires: None
+     * Modifies: None
+     * Effects: Gets the position struct
+    */
+    posStruct getPos() const;
+
+    /**
+     * Requires: None
+     * Modifies: None
+     * Effects: Gets the color struct
+    */
+    colorStruct getColor() const;
+
+    /**
+     * Requires: None
+     * Modifies: None
+     * Effects: Gets the food level
+    */
+    double getFoodLevel() const;
+
+    /**
+     * Requires: None
+     * Modifies: None
+     * Effects: Gets the water level
+    */
+    double getWaterLevel() const;
+
+    // Setters
+    /**
+     * Requires: Change amount
+     * Modifies: foodLevel
+     * Effects: Changes food level by the amount of 'change' positive or negative
+    */
+    void changeFoodLevel(double change);
+
+    /**
+     * Requires: Change amount
+     * Modifies: waterLevel
+     * Effects: Changes water level by the amount of 'change' positive or negative
+    */
+    void changeWaterLevel(double change);
+
+    /**
+     * Requires: Level
+     * Modifies: foodLevel
+     * Effects: Sets the food level to the level supplied
+    */
+    void setFoodLevel(double level);
+
+    /**
+     * Requires: Level
+     * Modifies: waterLevel
+     * Effects: Sets the water level to the level supplied
+    */
+    void setWaterLevel(double level);
 
 };
 
