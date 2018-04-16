e819f9f0546ea34767fc52912a9695706424b21d
maximombro <maximombro@gmail.com>
2018-04-16 17:34:37 -0400

    Added TextBased.cpp with main function

---
+++ textBased.cpp
@@ -0,0 +1,20 @@
+// Text Based Testing and File I/O
+/*
+ * How to use:
+ * 1. Go to CMakeLists.txt
+ * 2. Duplicate current 'add_executable(...)' line
+ * 3. Comment out original 'add_executable(...)' line with '#'
+ * 4. Remove '${SOURCE_FILES}' from new 'add_executable(...)' line
+ * 5. Add 'textBased.cpp' to new 'add_executable(...)' line
+ */
+
+int main() {
+    // Class Testing
+    // TODO: Implement class testing
+
+    // File I/O
+    // TODO: Implement File I/O for Save and Load
+
+    // Exit
+    return 0;
+}
