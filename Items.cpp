fb6bb5dc68ba6668929dfe31469e34848f13a188
chiefJota <jjthissell@gmail.com>
2018-04-19 11:18:07 -0400

    Created Items abstract class and subclasses FoodItem and WaterItem

--- Items.cpp
+++ Items.cpp
@@ -3,3 +3,177 @@
 //
 
 #include "Items.h"
+
+#include <regex>
+
+Item::Item() {
+
+}
+
+Item::Item(std::string &name, colorStruct color, posStruct pos) {
+    name = " ";
+    this->color = color;
+    this->pos = pos;
+}
+
+
+
+FoodItem::FoodItem(){
+
+}
+
+FoodItem::FoodItem(std::string &n, colorStruct color, posStruct pos) {
+    name = n;
+    this->color = color;
+    this->pos = pos;
+}
+
+
+std::string FoodItem::getItem() {
+    return name;
+}
+
+posStruct FoodItem::getPosition() {
+    return  pos;
+}
+
+colorStruct FoodItem::getColor() {
+    return color;
+}
+
+
+std::string FoodItem::setName(std::string food) {
+    name = food;
+}
+colorStruct FoodItem::setColor(colorStruct Color) {
+    color = Color;
+}
+
+posStruct FoodItem::setPos(posStruct position) {
+    pos = position;
+}
+
+bool::FoodItem::isMushroom() {
+
+    /*
+    * Random 1 to 100
+    * if Random <= 25
+    *      isPoisonous = true
+    * else
+    *      isP = false
+    */
+
+    srand(time(NULL));
+
+    int MIN_CHANCE = 1;
+    int MAX_CHANCE = 100;
+
+    int poisonChance = rand() % MAX_CHANCE + MIN_CHANCE;
+
+    if (poisonChance < 25){
+        isPoisonous = true;
+    }
+    else{
+        isPoisonous= false;
+    }
+
+    return isPoisonous;
+}
+
+
+WaterItem::WaterItem(){
+
+}
+
+WaterItem::WaterItem(std::string &n, colorStruct color, posStruct pos) {
+    name = n;
+    this->color = color;
+    this->pos = pos;
+}
+
+std::string WaterItem::getItem() {
+    return name;
+}
+
+posStruct WaterItem::getPosition() {
+    return pos;
+}
+
+colorStruct WaterItem::getColor() {
+    return color;
+}
+
+std::string WaterItem::setName(std::string water) {
+    name = water;
+}
+
+posStruct WaterItem::setPos(posStruct position) {
+    pos = position;
+}
+
+colorStruct WaterItem::setColor(colorStruct Color) {
+    color = Color;
+}
+
+bool::WaterItem::isMushroom() {
+
+    srand(time(NULL));
+
+    int MIN_CHANCE = 1;
+    int MAX_CHANCE = 100;
+
+    int poisonChance = rand() % MAX_CHANCE + MIN_CHANCE;
+
+    if (poisonChance < 25){
+        isPoisonous = true;
+    }
+    else{
+        isPoisonous= false;
+    }
+
+    return isPoisonous;
+}
+
+
+
+void FoodItem::testFoodItem(){
+
+    std::cout << "Food Item Before:" << this << std::endl;
+    setName("Berries");
+    //getters
+    std::cout << "Food Item name: " << getItem() << std::endl;
+    std::cout << "getPos: (" << getPosition().xPos << "," << getPosition().yPos << ")" << std::endl;
+    std::cout << "getColor: RGB(" << getColor().red << "," << getColor().green << "," << getColor().blue << ")" << std::endl;
+    std::cout << std::boolalpha << isMushroom() << std::endl;
+
+    name = "Pine-needles";
+    std::cout << "Food Item name: " << getItem() << std::endl;
+    setPos(posStruct(4, 5));
+    std::cout << "getPos: (" << getPosition().xPos << "," << getPosition().yPos << ")" << std::endl;
+    setColor(colorStruct(0.44, 1.0, 0.0));
+    std::cout << "getColor: RGB(" << getColor().red << "," << getColor().green << "," << getColor().blue << ")" << std::endl;
+    std::cout << std::boolalpha << isMushroom() << std::endl;
+}
+
+
+
+
+void WaterItem::testWaterItem(){
+
+    std::cout << "Water Item Before:" << this << std::endl;
+    setName("River Water");
+    //getters
+    std::cout << "Water name: " << getItem() << std::endl;
+    std::cout << "getPos: (" << getPosition().xPos << "," << getPosition().yPos << ")" << std::endl;
+    std::cout << "getColor: RGB(" << getColor().red << "," << getColor().green << "," << getColor().blue << ")" << std::endl;
+    std::cout << std::boolalpha << isMushroom() << std::endl;
+
+    name = "Distilled Water";
+    std::cout << "Water name: " << getItem() << std::endl;
+    setPos(posStruct(2, 1));
+    std::cout << "getPos: (" << getPosition().xPos << "," << getPosition().yPos << ")" << std::endl;
+    setColor(colorStruct(0.0, 0.0, 0.3));
+    std::cout << "getColor: RGB(" << getColor().red << "," << getColor().green << "," << getColor().blue << ")"
+              << std::endl;
+    std::cout << std::boolalpha << isMushroom() << std::endl;
+}
--- Items.h
+++ Items.h
@@ -1,16 +1,243 @@
 #ifndef CAMPINGSURVIVAL_ITEMS_H
 #define CAMPINGSURVIVAL_ITEMS_H
 
+#include <iostream>
+#include "Player.h"
+
 class Item {
+protected:
+    std::string name;
+    colorStruct color;
+    posStruct pos;
+    bool isPoisonous;
+
+public:
+
+    /**
+    * Default Constructor
+    */
+    Item();
+
+    /**
+     * Non-Default Constructor
+     * Requires: name, color, position
+     * Modifies: name, color, posiiton
+     * Effects: template for subclasses
+     * FoodItem and WaterItem
+    */
+    Item(std::string &name, colorStruct color, posStruct pos);
+
+    /**
+     * getters which are
+     * all pure virtual methods
+     * to be overriden
+     * @return
+     */
+    virtual std::string getItem() = 0;
+
+    virtual colorStruct getColor() = 0;
+
+    virtual posStruct getPosition() = 0;
+
+    /**
+     * setters which are all
+     * pure virtual
+     * to be overriden
+     * @param name
+     * @return
+     */
+    virtual std::string setName(std::string name) = 0;
+
+    virtual colorStruct setColor(colorStruct color) = 0;
+
+    virtual posStruct setPos(posStruct pos) = 0;
+
+    /**
+     * isMushroom function
+     * is pure virtual and is
+     * overriden in foodItem
+     * and WaterItem classes
+     */
+    virtual bool isMushroom() = 0;
 
 };
 
 class FoodItem : public Item {
 
+public:
+
+    /**
+    * Default Constructor
+    */
+    FoodItem();
+
+    /**
+    * Non-Default Constructor
+    * Requires: name, color, position
+    * Modifies: name, color, posiiton
+    * Effects: sets name, color, and spawning
+     * position of the food item
+   */
+    FoodItem(std::string &name, colorStruct color, posStruct pos);
+
+    /**
+     * Requires: Nothing
+     * Modifies: Nothing
+     * Effects: gets the position
+     * of the food item
+     *
+     */
+    posStruct getPosition() override ;
+
+
+    /**
+     * Requires: Nothing
+     * Modifies: Nothing
+     * Effects: gets the color
+     * of the food item
+     */
+    colorStruct getColor() override;
+
+    /**
+    * Requires: Nothing
+    * Modifies: Nothing
+    * Effects: gets the name
+    * of the food item
+    */
+    std::string getItem() override ;
+
+
+    /**
+    * Requires: Nothing
+    * Modifies: food item name
+    * Effects: sets the name
+    * of the food item
+    */
+    std::string setName(std::string n) override;
+
+    /**
+    * Requires: colorStruct color
+    * Modifies: food item color
+    * Effects: sets the Color
+    * of the food item
+    */
+    colorStruct setColor(colorStruct color) override;
+
+    /**
+    * Requires: posStruct
+    * Modifies: food item position
+    * Effects: sets the spawning positon
+    * of the food item
+    */
+    posStruct setPos(posStruct pos) override;
+
+    /**
+     * Requires: Nothing
+     * Modifies: determines if the
+     * food item is poisionous or not (true or false)
+     * Effects: the player will be affected
+     * by eating a fooditem that is poisonous
+     *
+     */
+    bool isMushroom() override;
+
+    /**
+     * Testing function for FoodItem class
+     */
+    void testFoodItem();
+
 };
 
 class WaterItem : public Item {
 
+public:
+
+    /**
+     * Default Constructor
+     */
+    WaterItem();
+
+    /**
+    * Non-Default Constructor
+    * Requires: name, color, position
+    * Modifies: name, color, posiiton
+    * Effects: sets name, color, and spawning
+    * position of the Water item
+    */
+    WaterItem(std::string &name, colorStruct color, posStruct pos);
+
+    /**
+    * Requires: Nothing
+    * Modifies: Nothing
+    * Effects: gets the name
+    * of the water item
+    */
+    std::string getItem() override;
+
+    /**
+     * Requires: Nothing
+     * Modifies: Nothing
+     * Effects: gets the position
+     * of the water item
+     *
+     */
+    posStruct getPosition() override;
+
+
+    /**
+    * Requires: Nothing
+    * Modifies: Nothing
+    * Effects: gets the color
+    * of the water item
+    */
+    colorStruct getColor() override;
+
+    /**
+    * Requires: Nothing
+    * Modifies: food item name
+    * Effects: sets the name
+    * of the water item
+    */
+    std::string setName(std::string n) override;
+
+
+    /**
+    * Requires: colorStruct color
+    * Modifies: food item color
+    * Effects: sets the Color
+    * of the water item
+    */
+    colorStruct setColor(colorStruct color) override;
+
+    /**
+    * Requires: posStruct pos
+    * Modifies: food item position
+    * Effects: sets the spawning positon
+    * of the water item
+    */
+    posStruct setPos(posStruct pos) override;
+
+    /**
+    * Requires: Nothing
+    * Modifies: determines if the
+    * food item is poisionous or not (true or false)
+    * Effects: the player will be affected
+    * by eating a fooditem that is poisonous
+    *
+    */
+    bool isMushroom() override;
+
+
+    /**
+     * Testing function for WaterItem
+     *
+     */
+    void testWaterItem();
+
+
 };
+
+
+
 
 #endif
